4f4f9f1f57eb4226e4239e40d625512ca5fec56f
Horv <tamasurfi@gmail.com>
2017-05-19 11:54:37 +0200

    Inital commit

---
+++ R/installR.R
@@ -0,0 +1,26 @@
+library("yaml")
+
+InstallLibs <- function(libs, load = TRUE) {
+  sapply(libs,
+         function(lib) {
+           if (!(lib %in% installed.packages()[, 1]))
+             install.packages(lib)
+           if (load)
+             library(lib, character.only = TRUE)
+         }
+  )
+}
+
+InstallLibsScript <- function(files, load = TRUE) {
+  contents <- sapply(files, function(file) readLines(file, file.size(file)))
+  libs <- c(gsub("library\\((.*)\\)", "\\1", contents)[grep("library", contents), ],
+            gsub("require\\((.*)\\)", "\\1", contents)[grep("require", contents), ])
+  libs <- gsub("[^[:alnum:]]", "", libs)
+  InstallLibs(libs, load)
+}
+
+
+InstallLibsYML <- function(file = "deps.yml") {
+  libs <- yaml.load_file(file)
+  InstallLibs(libs$deps, libs$load)
+}
